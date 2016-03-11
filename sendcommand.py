213db3820d60d25a4b6678d87c6c4f6ded548ce4
Danny Bauman <dannybauman@gmail.com>
2016-03-11 13:31:42 -0800

    script to send custom command via command line arg

---
+++ sendcommand.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import queuemanager
+import logging
+import sys
+
+logging.basicConfig()
+
+logger = logging.getLogger('blinkylog')
+hdlr = logging.FileHandler('log.txt')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.INFO)
+
+
+if __name__ == "__main__":
+  if len(sys.argv) <= 1:
+    logger.info("You must specify a command")
+  else:
+    command = sys.argv[1]
+    logger.info("Running a custom command " + command + "...")
+    queuemanager.addCommandToQueue(command)
